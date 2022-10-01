1a63a2f6eeefbeda59d271d9b3d9cff4218b1a48
artikov <oybekartikov@icloud.com>
2022-10-01 15:54:48 +0500

    responsiveness fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,11 +29,11 @@ const App = () => {
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justifyContent='space-between' alignItems='stretch' spacing={3}>
-                        <Grid item xs={12} sm={7}>
+                    <Grid className={classes.mainContainer} container justifyContent='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={12} md={8}>
                             <Posts setCurrentId={setCurrentId}/>
                         </Grid>
-                        <Grid item xs={12} sm={4}>
+                        <Grid item xs={12} sm={6} md={4}>
                             <Form  currentId={currentId} setCurrentId={setCurrentId}/>
                         </Grid>
                     </Grid>
