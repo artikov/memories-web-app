e55fbf6e33bbce44bfa70c02d54406835283622a
artikov <oybekartikov@icloud.com>
2022-10-12 17:30:21 +0500

    add middleware

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose"
+import express from "express"
 import PostMessage from "../models/postMessage.js"
+
+const router = express.Router()
 
 export const getPosts = async (req, res) => {
     try {
@@ -51,10 +54,25 @@ export const deletePost = async (req,res) => {
 export const likePost = async (req, res) => {
     const {id} = req.params
 
+    if(!req.userId) return res.json({message: 'Unauthenticated'})
+
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id')
 
     const post = await PostMessage.findById(id)
-    const updatedPost = await PostMessage.findByIdAndUpdate(id, {likeCount: post.likeCount + 1}, {new:true})
+
+    const index = post.likes.findIndex((id) => id == String(req.userId))
+
+    if (index === -1){
+        // like the post
+        post.likes.push(req.userId)
+    }else{
+        // remole like
+        post.likes = post.likes.filter((id) => id != String(req.userId))
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {new:true})
 
     res.json(updatedPost)
 }
+
+export default router
