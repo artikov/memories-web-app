62a05ff5dbaf51e8370b60c395a189c0e9362561
artikov <oybekartikov@icloud.com>
2023-10-30 17:58:32 +0500

    modifications

--- server/app.js
+++ server/app.js
@@ -1,33 +1,35 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-import postsRoutes from './routes/posts.js'
-import userRoutes from './routes/users.js'
+import postsRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 // APP INITIALIZATION
-const app = express()
+const app = express();
 
-dotenv.config({path:'config.env'})
+dotenv.config();
 
-app.use(bodyParser.json({limit: "30mb", extended: true}))
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
-app.use(cors())
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-app.use('/posts', postsRoutes)
-app.use('/user', userRoutes)
+app.use("/posts", postsRoutes);
+app.use("/user", userRoutes);
 
-app.use('/', (req, res) => {
-    res.send('Welcome to Memories API')
-})
-
+app.use("/", (req, res) => {
+	res.send("Welcome to Memories API");
+});
 
 // MONGODB CONFIGURATION
-const CONNECTION_URL = process.env.MONGO_URI
-const PORT = process.env.PORT || 5000
+const CONNECTION_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
-    .catch((err) => console.log(err.message))
+mongoose
+	.connect(CONNECTION_URL)
+	.then(() =>
+		app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+	)
+	.catch((err) => console.log(err.message));
